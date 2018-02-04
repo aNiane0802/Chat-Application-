43ca6dcd75763e71d7b72363dd7dc6e360c405cf
Aboubakrine Niane <aboubakrineniane@aboubakrines-macbook-pro.home>
2018-02-04 16:18:02 +0100

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Aboubakrine Niane on 04/02/2018.
+//  Copyright © 2018 Aboubakrine Niane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
